30b153c9362156be7cb2f809349bbc9a50b66b03
Janghj <jhj91_09@naver.com>
2020-01-28 18:06:46 +0900

    01_28_End_Update

--- swea/test.py
+++ swea/test.py
@@ -1,13 +1,26 @@
-#numbers =list(map(int, input().split(' ')))
-numbers = [3,2,1,4]
-
-sort_num = [0] * len(numbers)
-cnt = [0]*(max(numbers)+1)
-for number in numbers:
-    cnt[number] += 1
-for i in range(max(numbers)):
-    cnt[i+1] += cnt[i]
-for i in range(len(numbers)):
-    cnt[numbers[i]] -= 1
-    sort_num[cnt[numbers[i]]] = numbers[i]
-print(sort_num)
+T = 10
+for T in range(T):
+    N = int(input())
+    data = list()
+    for _ in range(100):
+        data.append( list(map(int, input().strip().split())) )
+    result = 0
+    
+    for i in range(100):
+        temp1, temp2= 0, 0
+        for k in range(100):
+            temp1 += data[i][k]
+            temp2 += data[k][i]
+        if temp1 > result:
+            result = temp1
+        if temp2 > result:
+            result = temp2
+    temp3, temp4 = 0, 0
+    for i in range(100):
+        temp3 += data[i][i]
+        temp4 += data[-i+1][-i+1]
+    if temp3 > result:
+        result = temp3
+    if temp4 > result:
+        result = temp4
+    print('#{} {}'.format(T+1, result))
