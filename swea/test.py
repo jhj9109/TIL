55cd128f6ce196e5e0bf90fd1947d6d584bbad6d
Janghj <jhj91_09@naver.com>
2020-01-29 19:48:14 +0900

    01_29_study

--- swea/test.py
+++ swea/test.py
@@ -1,26 +1,15 @@
-T = 10
+T = int(input())
 for T in range(T):
-    N = int(input())
-    data = list()
-    for _ in range(100):
-        data.append( list(map(int, input().strip().split())) )
-    result = 0
+    L, U, X = [n for n in map(int, input().strip().split())]
     
-    for i in range(100):
-        temp1, temp2= 0, 0
-        for k in range(100):
-            temp1 += data[i][k]
-            temp2 += data[k][i]
-        if temp1 > result:
-            result = temp1
-        if temp2 > result:
-            result = temp2
-    temp3, temp4 = 0, 0
-    for i in range(100):
-        temp3 += data[i][i]
-        temp4 += data[-i+1][-i+1]
-    if temp3 > result:
-        result = temp3
-    if temp4 > result:
-        result = temp4
+    result = 0
+    if X > U:
+        result = -1
+    else:
+        if X > L:
+            result = 0
+        else:
+            result = L-X
+        
+    
     print('#{} {}'.format(T+1, result))
