d764e5451d57f73801a14d1822bfcaa938d4c1b0
JangHJ <jhj91_09@naver.com>
2020-02-28 18:28:24 +0900

    swea_삼성시의버스노선

---
+++ 봄방학/6485/6485.py
@@ -0,0 +1,17 @@
+import sys
+sys.stdin = open('input6485.txt')
+#예제출력 #1 1 2 2 1 1
+T = int(input())
+for tc in range(1, T+1):
+    N = int(input())
+    buss = [list(int(x) for x in input().split())  for _ in range(N)]
+    P = int(input())
+    stops = [int(input()) for _ in range(P)]
+    res = []
+    for stop in stops:
+        cnt = 0
+        for bus in buss:
+            cnt += 1 if bus[0] <= stop <= bus[1] else 0
+        res.append(cnt)
+    print(f'#{tc}', end=' ')
+    print(*res, sep=' ')
