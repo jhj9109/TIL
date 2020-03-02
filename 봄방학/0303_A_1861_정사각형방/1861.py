0ed42639185545febb42b32b8fb8c1b3e7cd79be
JangHJ <jhj91_09@naver.com>
2020-03-02 19:00:25 +0900

    정사각형방_DFS_notvisted_w서치활용

--- 봄방학/0303_A_1861_정사각형방/1861.py
+++ 봄방학/0303_A_1861_정사각형방/1861.py
@@ -1,23 +1,25 @@
 import sys
 sys.stdin = open('input1861.txt')
 '''
-4:39~5:03
-좌표(i,j) 1~N^2
-+1 >>> 얼마나 많이
+abs 활용한 양쪽 퍼지기 >>> 유일한 경로 >>> not visited 활용가능
+한쪽 퍼지기 >>> 234 > 1234 가능하므로, 함수 호출전 출발지만 not visited 검사 
 '''
-def go(x, y):
+def go(x, y, res):
     s = [(x, y)]
-    ret = [field[x][y], 0]
+    temp = [field[x][y], 0]
     while s:
         x, y = s.pop()
-        if not visited[x][y]:
-            visited[x][y] = True
-            ret[1] += 1
-            ret[0] = min( ret[0], field[x][y] )
-            for k in range(4): 
-                if abs(field[x+dx[k]][y+dy[k]] - field[x][y]) == 1:
-                    s.append((x+dx[k], y+dy[k]))
-    return ret
+        visited[x][y] = True
+        temp[1] += 1
+        temp[0] = min( temp[0], field[x][y] )
+        for k in range(4): 
+            nx, ny = x+dx[k], y+dy[k]
+            if not visited[nx][ny] and abs(field[nx][ny] - field[x][y]) == 1:
+                s.append((nx, ny))
+    if res[1] < temp[1]:
+        res[0], res[1] = temp[0], temp[1]
+    elif res[1] == temp[1] and temp[0] < res[0]:
+        res[0] = temp[0]
 
 dx = [0, 0, 1,-1]
 dy = [1,-1, 0, 0]
@@ -31,9 +33,6 @@ for tc in range(1, 1+T):
     res = [N**2, 0] # 출발 방 넘버, 최대이동횟수
     for x in range(1, N+1):
         for y in range(1, N+1):
-            temp = go(x, y)
-            if res[1] < temp[1]:
-                res = temp
-            if res[1] == temp[1]:
-                res[0] = temp[0] if temp[0] < res[0] else res[0]
+            if not visited[x][y]:
+                go(x, y, res)
     print(f'#{tc} {res[0]} {res[1]}')
