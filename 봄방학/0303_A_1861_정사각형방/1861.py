cf8bf6f95d1892cdcedac1392c50f321f879e1da
JangHJ <jhj91_09@naver.com>
2020-03-02 18:33:18 +0900

    정사각형방_DFS_visited사용버전

--- 봄방학/0303_A_1861_정사각형방/1861.py
+++ 봄방학/0303_A_1861_정사각형방/1861.py
@@ -5,34 +5,35 @@ sys.stdin = open('input1861.txt')
 좌표(i,j) 1~N^2
 +1 >>> 얼마나 많이
 '''
-def go(X,Y):
-    x, y = X, Y
-    global N
-    global rx, ry, rv
-    cnt = 1
-    while True:
-        for k in range(4):
-            nx, ny = x+dx[k], y+dy[k]
-            if 0<=nx<=N-1 and 0<=ny<=N-1 and field[nx][ny] - field[x][y] == 1:
-                x, y = nx, ny
-                cnt += 1
-                break
-        else:
-            if cnt > rv or (cnt == rv and field[X][Y] < field[rx][ry]):
-                rv = cnt
-                rx, ry, rv = X, Y, cnt
-                return
-            else:
-                return
+def go(x, y):
+    s = [(x, y)]
+    ret = [field[x][y], 0]
+    while s:
+        x, y = s.pop()
+        if not visited[x][y]:
+            visited[x][y] = True
+            ret[1] += 1
+            ret[0] = min( ret[0], field[x][y] )
+            for k in range(4): 
+                if abs(field[x+dx[k]][y+dy[k]] - field[x][y]) == 1:
+                    s.append((x+dx[k], y+dy[k]))
+    return ret
 
 dx = [0, 0, 1,-1]
 dy = [1,-1, 0, 0]
 T = int(input())
 for tc in range(1, 1+T):
     N = int(input())
-    field = [list(map(int, input().split())) for _ in range(N)]
-    rx, ry, rv = -1, -1, -1
-    for x in range(N):
-        for y in range(N):
-            go(x,y)
-    print(f'#{tc} {field[rx][ry]} {rv}')
+    field = [ [-1] * (N+2) ]
+    field.extend([ [-1] + list(map(int, input().split())) + [-1] for _ in range(N)] )
+    field.append([-1]*(N+2))
+    visited = [ [False]*(N+2) for _ in range(N+2) ]
+    res = [N**2, 0] # 출발 방 넘버, 최대이동횟수
+    for x in range(1, N+1):
+        for y in range(1, N+1):
+            temp = go(x, y)
+            if res[1] < temp[1]:
+                res = temp
+            if res[1] == temp[1]:
+                res[0] = temp[0] if temp[0] < res[0] else res[0]
+    print(f'#{tc} {res[0]} {res[1]}')
