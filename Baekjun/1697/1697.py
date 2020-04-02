b5c1f0baa5512f0e64dce728b627d2626138c0a9
jhj9109 <jhj91_09@naver.com>
2020-04-02 22:29:45 +0900

    백준_숨바꼭질_BFS

---
+++ Baekjun/1697/1697.py
@@ -0,0 +1,33 @@
+'''9:55~
+0<=N,K<=100,000
+주인공 : 걷기, 순간이동
+걷기 : X±1
+순간이동 : X*2
+가장 빨리 찾는 시간?
+BFS
+'''
+import sys
+sys.stdin = open('input1697.txt')
+
+def go(N, K):
+    nk = abs(N-K)
+    v = [0]*(100001)
+    s = [(N,0)]
+    v[N] = 1
+    while s:
+        now, cnt = s.pop(0)
+        if now == K:
+            return cnt
+        else:
+            if now <= 99999 and v[now+1] == 0:
+                s.append((now+1, cnt+1))
+                v[now+1] = 1
+            if now >= 1 and v[now-1] == 0:
+                s.append((now-1, cnt+1))
+                v[now-1] = 1
+            if now <= 50000 and v[now*2] == 0:
+                s.append((now*2, cnt+1))
+                v[now*2] = 1
+
+N, K = map(int, input().split())
+print(go(N, K))
