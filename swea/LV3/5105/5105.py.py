6bbaf191d665af38ec9db215e2b618ca5c860dac
jhj9109 <jhj91_09@naver.com>
2020-04-03 12:55:34 +0900

    LV3_5105_use_BFS

--- swea/LV3/5105/5105.py.py
+++ swea/LV3/5105/5105.py.py
@@ -1,43 +1,38 @@
 import sys
-sys.stdin = open('sample_input.txt')
+sys.stdin = open("input5105_2.txt")
+'''
+0: 통로
+1: 벽
+2: 출발
+3: 도착
+'''
+def go(start):
+    v = [list(0 for _ in range(N)) for _ in range(N)]
+    q = [start]
+    v[start[0]][start[1]] = 1
+    while q:
+        x, y = q.pop(0)
+        if field[x][y] == '3':
+            return v[x][y]-2
+        else:
+            for k in range(4):
+                nx, ny = x+dx[k], y+dy[k]
+                if 0<=nx<N and 0<=ny<N and field[nx][ny] != '1' and not v[nx][ny]:
+                    q.append((nx, ny))
+                    v[nx][ny] = v[x][y] + 1
+    return 0
 
-def go(field, x, y, cnt, res):
-    dx = [0, 0, 1,-1]
-    dy = [1,-1, 0, 0]
-
-    for i in range(4): #4방향 서치
-        if (0<=x+dx[i]<=N-1) and (0<=y+dy[i]<=N-1): #필드경계 조건
-            if field[x+dx[i]][y+dy[i]] == 3: #도착
-
-                cnt[0] += 1
-                res.append(cnt[0]-1) #결과값에 지금까지 결과 덧셈
-                return
-
-            if field[x+dx[i]][y+dy[i]] == 0: #길 존재
-
-                field[x+dx[i]][y+dy[i]] = 1 #왔던길 삭제
-                cnt[0] += 1
-                go(field, x+dx[i], y+dy[i], cnt, res)
-
-                cnt[0] -= 1
-                field[x+dx[i]][y+dy[i]] = 0
-
-def return_start(field):
+def ret_start():
     for x in range(N):
         for y in range(N):
-            if field[x][y] == 2:
-                return (x, y)
+            if field[x][y] == '2':
+                return x, y
+
+dx = [0, 0, 1,-1]
+dy = [1,-1, 0, 0]
 
 T = int(input())
 for tc in range(1, T+1):
     N = int(input())
-    field = [list(int(x) for x in input()) for _ in range(N)]
-    (X, Y) = return_start(field)
-    
-    res = []
-    cnt = [0]
-    go(field, X, Y, cnt, res)
-    if len(res) == 0:
-        print(f'#{tc} 0')
-    else:
-        print(f'#{tc} {min(res)}')
+    field = [list(input()) for _ in range(N)]
+    print(f'#{tc} {go(ret_start())}')
