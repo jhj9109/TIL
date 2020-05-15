f5a9f12863d49a357f2426e35f72d14059565ab4
jhj <jhj91_09@naver.com>
2020-05-15 19:12:00 +0900

    change_use_iteral_set

--- 알고리즘수업/0515/5209. 최소생산비용/5209.py
+++ 알고리즘수업/0515/5209. 최소생산비용/5209.py
@@ -1,25 +1,23 @@
 import sys
 sys.stdin = open('input5209.txt')
 
-def go(i, cnt):
-    global res
-    if cnt >= res:
-        return
-    if i == N:
-        res = cnt
-        return
-    for j in range(N):
-        if not v[j]:
-            v[j] = 1
-            go(i+1, cnt+c[i][j])
-            v[j] = 0
-    return
+def go(N, c):
+    s = [ ( {n}, c[0][n]) for n in range(N) ] # 단계, 값
+    res = 99 * N
+    while s:
+        u, cnt = s.pop()
+        i = len(u)
+        if cnt >= res:
+            continue
+        if i == N:
+            res = cnt
+            continue
+        for j in set(range(N))-u:
+            s.append( (u.union({j}) , cnt + c[i][j]) )
+    return res
 
 T = int(input())
 for tc in range(1, T+1):
     N = int(input())
     c = [list(map(int, input().split())) for _ in range(N)] #cost
-    v = [0]*N 
-    res = 99 * N
-    go(0, 0)
-    print(f'#{tc} {res}')
+    print(f'#{tc} {go(N, c)}')
