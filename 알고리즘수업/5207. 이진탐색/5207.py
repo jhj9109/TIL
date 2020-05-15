91f958cabfcb84aca9d27aebcc34c3993a9c5f0e
jhj <jhj91_09@naver.com>
2020-05-16 01:05:16 +0900

    use_loop

--- 알고리즘수업/5207. 이진탐색/5207.py
+++ 알고리즘수업/5207. 이진탐색/5207.py
@@ -3,22 +3,22 @@ sys.stdin = open('input5207.txt')
 
 def go(n, l, r, v):
     global res
-    m = (l + r) // 2
-    if n == nums1[m]:
-        res += 1
-        return
-    if l == r:
-        return
-    if n > nums1[m]:
-        if v != 1:
-            go(n, m+1, r, 1)
-        else:
+    while True:
+        m = (l + r) // 2
+        if n == nums1[m]:
+            res += 1
             return
-    else:
-        if v != -1:
-            go(n, l, m-1, -1)
-        else:
+        if l == r:
             return
+        if n > nums1[m]:
+            if v != 1:
+                l, v = m+1, 1
+                continue
+        else:
+            if v != -1:
+                r, v = m-1, -1
+                continue
+        return
 
 T = int(input())
 for tc in range(1, T+1):
