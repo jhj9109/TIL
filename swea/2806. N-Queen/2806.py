57ffcdd63fc64eb020dbcf90212fb62c01313338
jhj <janghj18@gmail.com>
2020-05-28 14:56:07 +0900

    use DFS & dia

--- swea/2806. N-Queen/2806.py
+++ swea/2806. N-Queen/2806.py
@@ -1,22 +1,24 @@
 import sys
 sys.stdin = open("input2806.txt")
 
-def go(n, l, m, r):
+def go(n):
     global res
     if n == N:
         res += 1
         return
-    l = [x-1 for x in l if x-1 >= 0]
-    r = [x+1 for x in r if x+1 < N]
     for i in range(N):
-        if i not in l+m+r:
-            go(n+1, l+[i], m+[i], r+[i])
+        if not m[i] and not dia1[n+i] and not dia2[n-i+(N-1)]:
+            m[i] = dia1[n+i] = dia2[n-i+(N-1)] = 1
+            go(n+1)
+            m[i] = dia1[n+i] = dia2[n-i+(N-1)] = 0
     return     
-
 
 T = int(input())
 for tc in range(1, T+1):
     N = int(input())
     res = 0
-    go(0, [], [], [])
+    m = [0] * N
+    dia1 = [0] * (2*N-1) # 상향대각 : i+j : 0 ~ 2(N-1)
+    dia2 = [0] * (2*N-1) # 하향대각 : i-j : -(N-1) ~ (N-1) >>> (i-j)+(N-1)
+    go(0)
     print(f'#{tc} {res}')
