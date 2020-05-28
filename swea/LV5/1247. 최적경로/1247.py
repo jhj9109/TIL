a13394f38055881baaa99101fd57c7809ff3f568
jhj <janghj18@gmail.com>
2020-05-28 16:44:31 +0900

    use DFS

--- swea/LV5/1247. 최적경로/1247.py
+++ swea/LV5/1247. 최적경로/1247.py
@@ -1,34 +1,35 @@
 import sys
 sys.stdin = open('input1247.txt')
 
-from itertools import permutations
-def cal(lst):
+def dfs(k, now, dis):
     global res
-    cnt = 0
-
-    for n in range(N+1):
-        cnt += abs(people[lst[n]][0] - people[lst[n+1]][0]) + abs(people[lst[n]][1] - people[lst[n+1]][1])
-        if cnt > res:
-            return
-    res = cnt
-    return
+    if dis > res:
+        return
+    if k == N:
+        dis += abs(people[now][0]-home[0]) + abs(people[now][1]-home[1])
+        res = dis if dis < res else res
+        return
+    for w in range(1, N+1):
+        if not v[w]:
+            v[w] = 1
+            plus = abs(people[now][0]-people[w][0]) + abs(people[now][1]-people[w][1])
+            dfs(k+1, w, dis + plus)
+            v[w] = 0 
 
 T = int(input())
 for tc in range(1, T+1):
     N = int(input())
     people = []
-    res = 200 * (N+2)
-    lst = list(range(N))
-
-    for i,v in enumerate(list(map(int, input().split()))):
+    for i,v in enumerate(list(map(int, input().split()))): #회사 집 고객 N명
         if i&1:
             people.append( (temp, v))
         else:
             temp = v
+    home = people.pop(1)
+
+    res = float('inf')
+    v = [0] * (N+1)
+
+    dfs(0, 0, 0)
     
-    temp = people.pop(1)
-    people += [temp]
-    lst = list(range(1, N+1))
-    for permutation in permutations(lst, N):
-        cal([0]+list(permutation)+[N+1])
     print(f'#{tc} {res}')
