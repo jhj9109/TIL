37a3a659f005b65a75b3021d9f9c52ef95d0bd3d
jhj <janghj18@gmail.com>
2020-05-28 14:54:09 +0900

    update 2806

--- swea/2806. N-Queen/2806.py
+++ swea/2806. N-Queen/2806.py
@@ -1,12 +1,5 @@
-#팩토리얼 곱해지는 코드
 import sys
-sys.stdin = open("input.txt")
-
-def my_pacto(num):
-    if num == 1:
-        return 1
-    else:
-        return my_pacto(num-1) * num
+sys.stdin = open("input2806.txt")
 
 def cross(N, d, x, y):
     xx = [1, -1, 1, -1]
@@ -25,25 +18,23 @@ def cross(N, d, x, y):
 def chess(N, d, n):
     if n != 0:
         cnt = 0
-        for x in range(N):
-            for y in range(N):
-
-                if sum(d[x]) == 0 and sum((list(zip(*d))[y])) == 0 and cross(N, d, x, y):
-                    d[x][y] = 1
-                    cnt += chess(N, d, n-1)
-                    d[x][y] = 0
-                else:
-                    pass
-        return cnt
+        x = N - n
+        for y in range(N):
+            if sum(d[x][0:N+1]) == 0 and sum((list(zip(*d))[y])) == 0 and cross(N, d, x, y):
+                d[x][y] = 1
+                cnt += chess(N, d, n-1) #5->4....2->1
+                d[x][y] = 0
+            else:
+                pass
+        return cnt # n >= 1
     else:
-        return 1
+        return 1 # n == 0
+
 
 
 T = int(input())
 for tc in range(1, T+1):
     N = int(input())
-
     d = [[0] * N for _ in range(N)]
-    result = chess(N, d, N) // my_pacto(N)
-
+    result = chess(N, d, N)
     print(f'#{tc} {result}')
