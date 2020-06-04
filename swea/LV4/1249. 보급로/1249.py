28e2b7cc4a7a81cb180939c0997fbe76abe8a1fb
jhj <janghj18@gmail.com>
2020-06-04 14:02:09 +0900

    Use recursion dfs

--- swea/LV4/1249. 보급로/1249.py
+++ swea/LV4/1249. 보급로/1249.py
@@ -7,34 +7,34 @@ start_time = time.time()
 파여진 깊이만큼 복구 시간 증가
 복구 시간이 가장 짧은 경로는?
 '''
-# def dfs(r, c):
-#     global res
-#     if v[r][c] >= res:
-#         return
-#     if (r, c) == (N-1, N-1):
-#         res = v[r][c]
-#         return
-#     for dr, dc in [ (0,1), (0,-1), (-1,0), (1,0) ]:
-#         nr, nc = r+dr, c+dc
-#         if 0 <= nr < N and 0 <= nc < N and (not v[nr][nc] or (v[r][c] + f[nr][nc]) < v[nr][nc]):
-#             v[nr][nc] = v[r][c] + f[nr][nc]
-#             dfs(nr, nc)
-def dfs(N):
-    maxV = (2 * N - 3) * 9
-    v = [ [maxV]*N for _ in range(N)]
+def dfs(r, c):
+    if [r, c] == [N-1, N-1]:
+        return
+    for dr, dc in [ (0,1), (1,0), (0,-1), (-1,0) ]:
+        nr, nc = r+dr, c+dc
+        if 0 <= nr < N and 0 <= nc < N:
+            nValue = v[r][c] + f[nr][nc]
+            if nValue < v[N-1][N-1] and nValue < v[nr][nc]:
+                v[nr][nc] = nValue
+                dfs(nr, nc)
+# def dfs(N):
+#     maxV = (2 * N - 3) * 9
+#     v = [ [maxV]*N for _ in range(N)]
 
-    s = [(0, 0)]
-    v[0][0] = 0
-    while s:
-        r, c = s.pop()
-        for dr, dc in [ (0,1), (1,0), (0,-1), (-1,0) ]:
-            nr, nc = r+dr, c+dc
-            if 0 <= nr < N and 0 <= nc < N:
-                nValue = v[r][c] + f[nr][nc]
-                if nValue < v[N-1][N-1] and nValue < v[nr][nc]:
-                    s.append( (nr, nc) )
-                    v[nr][nc] = nValue
-    return v[N-1][N-1]
+#     s = [(0, 0)]
+#     v[0][0] = 0
+#     while s:
+#         r, c = s.pop()
+#         for dr, dc in [ (0,1), (1,0), (0,-1), (-1,0) ]:
+#             nr, nc = r+dr, c+dc
+#             if 0 <= nr < N and 0 <= nc < N:
+#                 nValue = v[r][c] + f[nr][nc]
+#                 if nValue < v[N-1][N-1] and nValue < v[nr][nc]:
+#                     s.append( (nr, nc) )
+#                     v[nr][nc] = nValue
+#     return v[N-1][N-1]
+
+
 
 # from collections import deque
 # def bfs(N):
@@ -61,6 +61,11 @@ for tc in range(1, T+1):
     f = [list(map(int, input())) for _ in range(N)]
     # f[0][0], f[N-1][N-1] = 0, 0
     # 맵 d에 대응하는 v 작성 > 가는데까지 걸린 소요시간 반영
-    print(f'#{tc} {dfs(N)}')
+
+    maxV = (2 * N - 3) * 9
+    v = [ [maxV]*N for _ in range(N)]
+    v[0][0] = 0
+    dfs(0, 0)
+    print(f'#{tc} {v[N-1][N-1]}')
 
 print(time.time() - start_time, 'seconds')
