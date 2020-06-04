28aecaf8377b0635932d7f20bf4ad4f833e70ada
jhj <janghj18@gmail.com>
2020-06-04 16:49:44 +0900

    Dijkstra_no_heap_use_set

--- swea/LV4/1249. 보급로/1249.py
+++ swea/LV4/1249. 보급로/1249.py
@@ -3,29 +3,43 @@ sys.stdin = open('input1249.txt')
 import time
 start_time = time.time()
 
-from collections import deque
-def bfs(r, c):
+def Dijkstra():
     maxV = (2 * N - 3) * 9
     v = [ [maxV]*N for _ in range(N)]
+    
+    #초기값
+    v[0][0] = 0
+    w = (0, 0) # w선택
+    U = set()
+    U.add(w)
+    v[0][1] = f[0][1] # w의 인접정점 갱신
+    v[1][0] = f[1][0]
+    
+    while len(U) != N*N:
+        # w선택
+        minV = (2 * N - 3) * 9 + 1
+        for u in U:
+            for dr, dc in [ (0,1), (1,0), (0,-1), (-1,0) ]:
+                nr, nc = u[0]+dr, u[1]+dc
+                if 0 <= nr < N and 0 <= nc < N and (nr, nc) not in U and v[nr][nc] < maxV:
+                    minV = v[nr][nc]
+                    w = (nr, nc)
+        U.add(w)
 
-    q = deque( [[r, c]] )
-    v[r][c] = 0
-    while q:
-        r, c = q.popleft()
-        for dr, dc in [ (0,1), (0,-1), (-1,0), (1,0) ]:
-            nr, nc = r+dr, c+dc
-            if 0 <= nr < N and 0 <= nc < N:
-                nValue = v[r][c]+f[nr][nc]
-                if nValue < min(v[nr][nc], v[N-1][N-1]) :
-                    v[nr][nc] = nValue
-                    if (r, c) != (N-1, N-1):
-                        q.append( [nr, nc] )
+        # w 인접정점 갱신
+        for dr, dc in [ (0,1), (1,0), (0,-1), (-1,0) ]:
+            nr, nc = w[0]+dr, w[1]+dc
+            if 0 <= nr < N and 0 <= nc < N and (nr, nc) not in U:
+                nValue = v[w[0]][w[1]] + f[nr][nc]
+                v[nr][nc] = nValue if nValue < v[nr][nc] else v[nr][nc]
+
     return v[N-1][N-1]
 
 T = int(input())
 for tc in range(1, T+1):
     N = int(input())
     f = [list(map(int, input())) for _ in range(N)]
-    print(f'#{tc} {bfs(0, 0)}')
+
+    print(f'#{tc} {Dijkstra()}')
 
 print(time.time() - start_time, 'seconds')
