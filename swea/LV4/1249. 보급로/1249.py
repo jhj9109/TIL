b8fc813c15902b5f92b9c203575b1cc106dd4146
jhj <janghj18@gmail.com>
2020-06-04 16:50:34 +0900

    Dijkstra_no_heap_use_u

--- swea/LV4/1249. 보급로/1249.py
+++ swea/LV4/1249. 보급로/1249.py
@@ -3,33 +3,34 @@ sys.stdin = open('input1249.txt')
 import time
 start_time = time.time()
 
-def Dijkstra():
+def Dijkstra(): #힙 미사용
     maxV = (2 * N - 3) * 9
     v = [ [maxV]*N for _ in range(N)]
+    u = [ [0] * N for _ in range(N)]
     
     #초기값
     v[0][0] = 0
     w = (0, 0) # w선택
-    U = set()
-    U.add(w)
+    u[0][0] = 1
+    cnt = 1
     v[0][1] = f[0][1] # w의 인접정점 갱신
     v[1][0] = f[1][0]
     
-    while len(U) != N*N:
+    while cnt != N*N:
         # w선택
-        minV = (2 * N - 3) * 9 + 1
-        for u in U:
-            for dr, dc in [ (0,1), (1,0), (0,-1), (-1,0) ]:
-                nr, nc = u[0]+dr, u[1]+dc
-                if 0 <= nr < N and 0 <= nc < N and (nr, nc) not in U and v[nr][nc] < maxV:
-                    minV = v[nr][nc]
-                    w = (nr, nc)
-        U.add(w)
+        minV = float('inf')
+        for r in range(N):
+            for c in range(N):
+                if not u[r][c] and v[r][c] < minV:
+                    minV = v[r][c]
+                    w = (r, c)
+        u[w[0]][w[1]] = 1
+        cnt += 1
 
         # w 인접정점 갱신
         for dr, dc in [ (0,1), (1,0), (0,-1), (-1,0) ]:
             nr, nc = w[0]+dr, w[1]+dc
-            if 0 <= nr < N and 0 <= nc < N and (nr, nc) not in U:
+            if 0 <= nr < N and 0 <= nc < N and not u[nr][nc]:
                 nValue = v[w[0]][w[1]] + f[nr][nc]
                 v[nr][nc] = nValue if nValue < v[nr][nc] else v[nr][nc]
 
