cd692da34e48a6571602f681f9c63509c4d1d317
jhj <janghj18@gmail.com>
2020-06-04 13:55:08 +0900

    del res

--- swea/LV4/1249. 보급로/1249.py
+++ swea/LV4/1249. 보급로/1249.py
@@ -1,5 +1,7 @@
 import sys
 sys.stdin = open('input1249.txt')
+import time
+start_time = time.time()
 '''10:50
 출발지 S(0,0) > 도착지 G(N-1,N-1) 까지 가기 위한 도로복구 빠른 시간내 수행
 파여진 깊이만큼 복구 시간 증가
@@ -18,26 +20,21 @@ sys.stdin = open('input1249.txt')
 #             v[nr][nc] = v[r][c] + f[nr][nc]
 #             dfs(nr, nc)
 def dfs(N):
-    v = [ [0]*N for _ in range(N)]
-    visited =  [ [0]*N for _ in range(N)]
-    res = (2 * N - 3) * 9
+    maxV = (2 * N - 3) * 9
+    v = [ [maxV]*N for _ in range(N)]
 
     s = [(0, 0)]
-    visited[0][0] = 1
+    v[0][0] = 0
     while s:
         r, c = s.pop()
         for dr, dc in [ (0,1), (1,0), (0,-1), (-1,0) ]:
             nr, nc = r+dr, c+dc
             if 0 <= nr < N and 0 <= nc < N:
-                if (nr, nc) == (N-1, N-1):
-                    res = v[r][c] if v[r][c] < res else res
-                else:
-                    nValue = v[r][c] + f[nr][nc]
-                    if not visited[nr][nc] or ( nValue < res and nValue < v[nr][nc] ):
-                        s.append( (nr, nc) )
-                        visited[nr][nc] = 1
-                        v[nr][nc] = nValue
-    return res
+                nValue = v[r][c] + f[nr][nc]
+                if nValue < v[N-1][N-1] and nValue < v[nr][nc]:
+                    s.append( (nr, nc) )
+                    v[nr][nc] = nValue
+    return v[N-1][N-1]
 
 # from collections import deque
 # def bfs(N):
@@ -65,3 +62,5 @@ for tc in range(1, T+1):
     # f[0][0], f[N-1][N-1] = 0, 0
     # 맵 d에 대응하는 v 작성 > 가는데까지 걸린 소요시간 반영
     print(f'#{tc} {dfs(N)}')
+
+print(time.time() - start_time, 'seconds')
