e4791cd3d868e842e024372c0a4df6099b147c27
jhj <janghj18@gmail.com>
2020-06-04 16:50:59 +0900

    Dijkstra Use Heap

--- swea/LV4/1249. 보급로/1249.py
+++ swea/LV4/1249. 보급로/1249.py
@@ -3,37 +3,33 @@ sys.stdin = open('input1249.txt')
 import time
 start_time = time.time()
 
-def Dijkstra(): #힙 미사용
+import heapq
+
+def Dijkstra(): #힙사용
     maxV = (2 * N - 3) * 9
     v = [ [maxV]*N for _ in range(N)]
     u = [ [0] * N for _ in range(N)]
-    
+    pq = []
+
     #초기값
     v[0][0] = 0
-    w = (0, 0) # w선택
-    u[0][0] = 1
-    cnt = 1
-    v[0][1] = f[0][1] # w의 인접정점 갱신
-    v[1][0] = f[1][0]
+    heapq.heappush(pq, ( v[0][0], (0,0) ) )
     
-    while cnt != N*N:
-        # w선택
-        minV = float('inf')
-        for r in range(N):
-            for c in range(N):
-                if not u[r][c] and v[r][c] < minV:
-                    minV = v[r][c]
-                    w = (r, c)
-        u[w[0]][w[1]] = 1
-        cnt += 1
+    while pq:
+        # 최소값 찾기
+        val, (r, c) = heapq.heappop(pq) 
+        
+        # u로 선택
+        u[r][c] = 1
 
         # w 인접정점 갱신
         for dr, dc in [ (0,1), (1,0), (0,-1), (-1,0) ]:
-            nr, nc = w[0]+dr, w[1]+dc
+            nr, nc = r+dr, c+dc
             if 0 <= nr < N and 0 <= nc < N and not u[nr][nc]:
-                nValue = v[w[0]][w[1]] + f[nr][nc]
-                v[nr][nc] = nValue if nValue < v[nr][nc] else v[nr][nc]
-
+                nValue = v[r][c] + f[nr][nc]
+                if nValue < v[nr][nc]:
+                    v[nr][nc] = nValue
+                    heapq.heappush(pq, (v[nr][nc], (nr,nc) ) )
     return v[N-1][N-1]
 
 T = int(input())
