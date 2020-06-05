849188c2e8f2416d72b6aa2b8d160cec14e186c7
jhj <janghj18@gmail.com>
2020-06-05 12:26:46 +0900

    Edit text2

--- 봄방학/0228_1486_장훈이의높은선반/1486.py
+++ 봄방학/0228_1486_장훈이의높은선반/1486.py
@@ -2,39 +2,31 @@ import sys
 sys.stdin = open('input1486.txt')
 import time
 start_time = time.time()
+
 from itertools import combinations
 
-def total_sub(item):
-    global result
-    h = max_height
-    lst = list(item)
-    for l in lst:
-        h -= l
-        if h < B:
-            return False
-    result = h if h < result else result
-    return True
-
-def go(): # 점원수, 탑의높이, 전체 직원의 키높이
+def go(N, B, max_height): # 점원수, 탑의높이, 전체 직원의 키높이
+    result = max_height
     for r in range(N+1):
         flag = True
-        for iterable in combinations(height, r): #combi : 원소 r개 짜리 집합 , 각 원소 이터러블 >>> sum
-            if total_sub(iterable):
-                flag = False
+        for lst in combinations(height, r): #combi : 원소 r개 짜리 집합 , 각 원소 이터러블 >>> sum
+            temp = max_height - sum(lst)
+            if temp < B:
+                continue
+            flag = False
+            result = temp if temp < result else result
 
         if flag: # 더 이상 만족할수 있는 케이스 X
             break
+    return result - B
 
 T = int(input())
-T = 5
 for tc in range(1, T+1):
     N, B = map(int, input().split())
     height = list(map(int, input().split()))
 
     max_height = sum(height)
-    result = max_height
 
-    go()
-    print(f'#{tc} {result-B}')
+    print(f'#{tc} {go(N, B, max_height)}')
 
 print(time.time() - start_time, 'seconds')
