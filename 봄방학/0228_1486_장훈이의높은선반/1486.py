90df2f4ba726ec40dd8ad4fa1edb664334615622
jhj <janghj18@gmail.com>
2020-06-05 12:57:55 +0900

    Use my powerset

--- 봄방학/0228_1486_장훈이의높은선반/1486.py
+++ 봄방학/0228_1486_장훈이의높은선반/1486.py
@@ -5,20 +5,26 @@ start_time = time.time()
 
 from itertools import combinations
 
+def powerset(N, k, r, l, h): # k:depth, r: 목표 원소 갯수 l: 현재 원소 갯수, sums: 값
+    global result, flag
+    if h < B:
+        return
+    if r == l:
+        result = h if h < result else result
+        flag = False
+        return
+    if N == k:
+        return
+    powerset(N, k+1, r, l+1, h-height[k])
+    powerset(N, k+1, r, l, h)
+
 def go(N, B, max_height): # 점원수, 탑의높이, 전체 직원의 키높이
-    result = max_height
+    global flag
     for r in range(N+1):
         flag = True
-        for lst in combinations(height, r): #combi : 원소 r개 짜리 집합 , 각 원소 이터러블 >>> sum
-            temp = max_height - sum(lst)
-            if temp < B:
-                continue
-            flag = False
-            result = temp if temp < result else result
-
+        powerset(N, 0, r, 0, max_height)
         if flag: # 더 이상 만족할수 있는 케이스 X
             break
-    return result - B
 
 T = int(input())
 for tc in range(1, T+1):
@@ -26,7 +32,10 @@ for tc in range(1, T+1):
     height = list(map(int, input().split()))
 
     max_height = sum(height)
+    result = max_height
+    flag = True
 
-    print(f'#{tc} {go(N, B, max_height)}')
+    go(N, B, max_height)
+    print(f'#{tc} {result - B}')
 
 print(time.time() - start_time, 'seconds')
