ff68d6c11eee47c1ffc2a8e01afebdeae5d196a4
jhj <janghj18@gmail.com>
2020-06-05 10:18:57 +0900

    Use my Queue

--- swea/LV5/5521. 상원이의 생일파티/5521.py
+++ swea/LV5/5521. 상원이의 생일파티/5521.py
@@ -1,26 +1,34 @@
 import sys
 sys.stdin = open('input5521.txt')
 
-from collections import deque
-
 def bfs(node):
-    global cnt
-    q = deque([node])
+    global cnt, f, r
+
+    r += 1
+    Q[r] = node # enQeueu
     V[node] = 1
     # cnt += 1 상원이 본인은 카운트 X
-    while q:
-        now = q.popleft()
-        if V[now] < 3:
-            for w in G[now]:
-                if not V[w]:
-                    q.append(w)
-                    V[w] = V[now]+1
-                    cnt += 1
+
+    while (f != r): # Not Empty
+        f += 1
+        now = Q[f]
+        if V[now] >= 3:
+            return
+        for w in G[now]:
+            if not V[w]:
+                r += 1
+                Q[r] = i
+                V[w] = V[now]+1
+                cnt += 1
 
 T = int(input())
 for tc in range(1, T+1):
     N, M = map(int, input().split())
     G = [[] for _ in range(N+1)]
+    
+    Q = [0] * (N*N)
+    f = r = -1
+
     V = [0]*(N+1)
 
     for i in range(M):
