31bfa359025b138849156c019a8b4e4892ed3f0e
jhj <janghj18@gmail.com>
2020-06-25 18:21:55 +0900

    Use BFS | 로컬 예제확인 | 서버 메모리초과

--- Baekjun/15684_사다리조작/15684.py
+++ Baekjun/15684_사다리조작/15684.py
@@ -1,52 +1,60 @@
 import sys
 sys.stdin = open('input15684.txt')
-sys.setrecursionlimit(3000) 
-def game(cnt):
+
+from collections import deque
+
+def game(lst):
     global res
     for c in range(1, N+1):
         now = c
         for r in range(H):
-            if bridge[r][now-1]:
+            if (r, now-1) in lst:
                 now -= 1
-            elif bridge[r][now]:
+            elif (r, now) in lst:
                 now += 1
             # else: now = now
         if now != c:
-            return
-    res = cnt
+            return False
+    return True
 
-def sim(col, row, cnt):
-    if cnt >= res:
-        return
-    game(cnt)
+def sim_bfs(init_lst):
+    if game(init_lst):
+        return 0
 
-    # 해당 col의 모든 row에 새로운 다리를 놓는다
-    for c in range(col, N):
+    dq = deque([ init_lst ])
+
+    while dq:
+        lst = dq.popleft()
+
+        if game(lst):
+            return len(lst) - len(init_lst)
+
+        if len(lst) == len(init_lst):
+            row, col = -1, -1 
+        else:
+            row, col = lst[-1]
+
+        # 새로운 다리 놓을 후보
         for r in range(H):
-            if (c > col or r > row) and not bridge[r][c-1] and not bridge[r][c] and not bridge[r][c+1]:
-                bridge[r][c] = 1
-                sim(c, r, cnt+1)
-                bridge[r][c] = 0
-                sim(c, r, cnt)
-
+            for c in range(col, N):
+                if (c > col or r > row) and (r, c-1) not in lst and (r, c) not in lst and (r, c+1) not in lst:
+                    dq.append( lst+[(r, c)] )
+    else:
+        return -1
 
 T = 7
 for tc in range(1, T+1):
     N, M, H = map(int, input().split())
 
-    bridge = [[0]*(N+2) for _ in range(H)]
+    init_lst = []
+    v = [[0]*(N+2) for _ in range(H)]
+    
 
     for _ in range(M):
         # a:row, b:column
         a, b = map(int, input().split())
-        bridge[a-1][b] = 1
+        init_lst.append( (a-1, b) )
 
     res = float('inf')
 
-    # 1번 세로줄부터 가능한 다리 모두 놓는다 => 재귀
-
-    sim(1, 0, 0)
-
-    res = -1 if res == float('inf') else res
-    print(f'{res}')
-
+    print(f'#{tc} {sim_bfs(init_lst)}')
